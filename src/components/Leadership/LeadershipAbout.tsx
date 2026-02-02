9ee20753bc307dce16d7c4e7a760492210e4c0bb
Salmanfarzee <salmanfarzee00010@gmail.com>
2026-02-02 11:20:35 +0530

    leadership content changes

--- src/components/Leadership/LeadershipAbout.tsx
+++ src/components/Leadership/LeadershipAbout.tsx
@@ -2,7 +2,7 @@ import styles from "./Leadership.module.scss";
 
 const LeadershipAbout = () => {
   return (
-    <section className="grid md:grid-cols-2 grid-cols-1 my-12">
+    <section className="grid md:grid-cols-[60%_40%] grid-cols-1 my-12">
       {/* Content on the left */}
       <div className="ml-[6.9vw]">
         <div className="flex items-center md:items-start flex-col mt-[2.7vh]">
@@ -14,17 +14,25 @@ const LeadershipAbout = () => {
           {/* Desktop Content */}
           <p className={`${styles.traincondent} mt-[26px] md:block hidden`}>
             midas360 (Metrorail Intelligent Decision Analytics System) is an
+            <br />
             advanced AI-powered analytics platform designed to optimize metro
+            <br />
             rail operations and maintenance. Built to support the complex
+            <br />
             demands of modern metro rail networks, midas360 leverages real-time
+            <br />
             data, machine learning, and predictive intelligence to transform how
+            <br />
             rail systems are managed—from infrastructure monitoring to asset
+            <br />
             performance and service reliability.
           </p>
           <p className={`${styles.traincondent} mt-[40px] md:block hidden`}>
             At its core, midas360 integrates data from various sources internal
+            <br />
             systems, IoT sensors, and third-party platforms to deliver
-            actionable insights that empower operators to make informed .
+            actionable <br />
+            insights that empower operators to make informed .
           </p>
 
           {/* Mobile Title */}
@@ -37,25 +45,32 @@ const LeadershipAbout = () => {
             className={`${styles.traincondent} mt-[12px] block md:hidden justify-center text-center pb-12`}
           >
             midas360 (Metrorail Intelligent Decision Analytics System) is an
-            advanced AI-powered analytics platform designed to optimize metro
-            rail operations and maintenance. Built to support the complex
-            demands of modern metro rail networks, midas360 leverages real-time
-            data, machine learning, and predictive intelligence to transform how
-            rail systems are managed—from infrastructure monitoring to asset
-            performance and service reliability.
+            advanced <br />
+            AI-powered analytics platform designed to optimize metro rail
+            operations
+            <br /> and maintenance. Built to support the complex demands of
+            modern metro
+            <br /> rail networks, midas360 leverages real-time data, machine
+            learning, and
+            <br /> predictive intelligence to transform how rail systems are
+            managed—from
+            <br /> infrastructure monitoring to asset performance and service
+            reliability.
           </p>
           <p
             className={`${styles.traincondent} mt-[20px] block md:hidden justify-center text-center pb-12`}
           >
             At its core, midas360 integrates data from various sources internal
+            <br />
             systems, IoT sensors, and third-party platforms to deliver
-            actionable insights that empower operators to make informed .
+            actionable
+            <br /> insights that empower operators to make informed .
           </p>
         </div>
       </div>
 
       {/* Image on the right */}
-      <div className="md:flex hidden justify-end mr-[6.9vw]">
+      <div className="md:flex hidden justify-center mr-[6.9vw]">
         <img
           src="/assets/train-leadership-right.svg"
           alt="Metro Rails Icon"
